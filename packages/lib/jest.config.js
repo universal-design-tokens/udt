56de062a7f914626e19da43ef84bf965ec347737
James Nash <cirrus@twiddles.com>
2019-06-30 19:19:45 +0100

    Changed lib build from Babel to TS only.

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  testPathIgnorePatterns: [
-    '<rootDir>/lib/',
-    '<rootDir>/node_modules/',
-  ],
-  testRegex: "\\.test\\.ts$",
-  transform: {
-    "^.+\\.(ts)$": "<rootDir>/node_modules/babel-jest",
-  },
-  moduleFileExtensions: [
-    "ts",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  preset: 'ts-jest',
+  rootDir: 'src/',
   collectCoverageFrom: [
     'src/**/*.ts',
   ],
