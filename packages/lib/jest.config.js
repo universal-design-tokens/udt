bde26683de4610a2eb14db888068eb84a4da982a
James Nash <cirrus@twiddles.com>
2022-08-17 00:02:56 +0100

    chore: removes old lib package

--- packages/lib/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-  ],
-};
--- packages/lib/src/base/node-with-parent.ts
+++
@@ -1,5 +0,0 @@
-import NodeWithChildren, { ChildKey } from './node-with-children';
-
-export default interface NodeWithParent {
-  getParent(): NodeWithParent & NodeWithChildren<NodeWithParent, ChildKey> | undefined;
-}
